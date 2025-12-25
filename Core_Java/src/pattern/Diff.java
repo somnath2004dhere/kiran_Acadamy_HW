52f61b66f382a99b043bc4377f5f897a625ae288
somnath <somnath2004dhere@gmail.com>
2025-12-25 21:58:22 +0530

    code add

---
+++ Core_Java/src/pattern/Diff.java
@@ -0,0 +1,43 @@
+package pattern;
+
+public class Diff {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		
+		for (int i = 0; i <n; i++) {
+			
+			
+			for (int j = 0; j <=i; j++) {
+				System.out.print("*");
+			}
+			
+			for (int j = 0; j <4*(n-i); j++) {
+				System.out.print(" ");
+				
+			}
+			
+			for (int j = 0; j <=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		n=2*n;
+for (int i = 0; i <n; i++) {
+			
+			for (int j = 0; j <i; j++) {
+				
+				System.out.print(" ");
+				
+			}
+			for (int j = 0; j <n-i; j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			
+		}
+	}
+}
---
+++ Core_Java/src/pattern/Floyd_Triangle.java
@@ -0,0 +1,29 @@
+package pattern;
+
+public class Floyd_Triangle {
+	
+	
+	public static void inverseNumber(int n) {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int n=10;
+		
+		
+		
+		for (int i = 0; i < n; i++) {
+			
+			int a=1;
+			for (int j = 0; j < n-i; j++) {
+				System.out.print(a++ +" ");
+				
+			}
+        System.out.println();
+			
+		}
+	}
+
+}
